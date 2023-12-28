4b52317dad949bc3e20379a05918591be310f12f
cyl19970726 <15258378443@163.com>
2023-12-28 14:19:29 +0800

    fix bug of pippenger

--- src/pockets/mod.rs
+++ src/pockets/mod.rs
@@ -1,8 +1,8 @@
 mod error;
 
+use ark_ec::CurveGroup;
 use std::ops::BitAnd;
 use std::ptr::null;
-use ark_ec::CurveGroup;
 use std::u64;
 
 use error::PocketError;
@@ -21,7 +21,10 @@ pub use multiproof::{
     MultiProof, MultiProofParameters, MultiProofPolyCommit, MultiProofPolyCommit2,
 };
 
-use ark_ff::{BigInt, BigInteger, BigInteger128, BigInteger320, BigInteger64, CyclotomicMultSubgroup, Field, One, PrimeField};
+use ark_ff::{
+    BigInt, BigInteger, BigInteger128, BigInteger320, BigInteger64, CyclotomicMultSubgroup, Field,
+    One, PrimeField,
+};
 
 mod asvc;
 pub use asvc::{ASvccommit, ASvckey, ASvcproof};
@@ -71,6 +74,7 @@ pub fn pippenger<E: Pairing>(
     // i row j column
     // k_0,0 k_0,1 k_0,2 ==> n_ki[0]
     // k_1,0 k_1,1 k_1,2 ==> n_ki[1]
+    // k_2,0 k_2,1 k_2,2 ==> n_ki[1]
     let n_ki: Vec<Vec<u32>> = exponents
         .into_iter()
         .map(|v| split_to_nbit_uint_form::<E::ScalarField>(v, N_BIT))
@@ -82,19 +86,19 @@ pub fn pippenger<E: Pairing>(
     let mut window_index_set_vec: Vec<Vec<u32>> = Vec::new();
     let mut split_nums = n_ki[0].len();
     // check the k_i numbers
-    n_ki.clone().into_iter().for_each(|vec|{
-        if vec.len() != split_nums{
+    n_ki.clone().into_iter().for_each(|vec| {
+        if vec.len() != split_nums {
             panic!("err k_i numbers")
         }
     });
 
-    for i in 0..split_nums{
+    for i in 0..split_nums {
         window_index_set_vec.push(Vec::new());
     }
 
-    // k_0,0 k_1,0 window_index_set_vec[0]
-    // k_0,1 k_1,1 window_index_set_vec[1]
-    // k_0,2 k_1,2 window_index_set_vec[2]
+    // k_0,0 k_1,0 k_2,0  window_index_set_vec[0]
+    // k_0,1 k_1,1 k_2,1 window_index_set_vec[1]
+    // k_0,2 k_1,2 k_2,2 window_index_set_vec[2]
     n_ki.into_iter().for_each(|value| {
         for j in 0..split_nums {
             window_index_set_vec[j].push(value[j]);
@@ -103,56 +107,66 @@ pub fn pippenger<E: Pairing>(
 
     // Calculate the each S_i set different numbers of Pi
     // NOTICE: we no need to record the 0 number element
-    let result_g =
-        window_index_set_vec
-            .into_iter()
-            .enumerate()
-            .fold(E::G1::zero(), |acc_B, (j, value)| {
-                // change to array
-                // let mut lamada_to_P = Vec::with_capacity(2^N_BIT);
-                let mut lamada_to_P: [Vec<usize>; 2_usize.pow(N_BIT as u32)] = std::array::from_fn(|_| Vec::new());
+    let result_g = window_index_set_vec.into_iter().enumerate().fold(
+        E::G1::zero().into(),
+        |acc_B, (j, window_value)| {
+            // change to array
+            // the max number for lambda is 2^N_Bit -1 which equal to the window size
+            let mut lambda_to_P: [Vec<usize>; 2_usize.pow(N_BIT as u32)] =
+                std::array::from_fn(|_| Vec::new());
 
-                value.into_iter().enumerate().for_each(
-                    // lamada express the coefficient for each P_i
-                    |(i,lamada)| {
-                        lamada_to_P[lamada as usize].push(i);
-                    },
-                );
+            // we collect different P_i with the same lambda into a Vector 'lambda_to_P[lambda]'
+            // if the j equal to 0, the window_value is the first window which consist of [k_0,0 k_1,0 k_2,0] the first index is i and the second index is j
+            window_value.into_iter().enumerate().for_each(
+                // lambda express the coefficient for each P_i
+                |(i, lambda)| {
+                    if lambda != 0 {
+                        lambda_to_P[lambda as usize].push(i);
+                    }
+                },
+            );
 
-                // Calculate T Set
-                let mut T: [E::G1Affine;2_usize.pow(N_BIT as u32)] = std::array::from_fn(|_| E::G1::zero().into());
-                for index in 0..lamada_to_P.len() {
-                    let s_index = lamada_to_P.len() - 1 - index;
-                    T[index] = lamada_to_P[s_index]
+            // Calculate T Set
+            let mut T: [E::G1Affine; 2_usize.pow(N_BIT as u32)] =
+                std::array::from_fn(|_| E::G1::zero().into());
+            for index in 0..lambda_to_P.len() {
+                let s_index = lambda_to_P.len() - 1 - index;
+                if lambda_to_P[s_index].len() == 0 {
+                    T[index] = E::G1::zero().into();
+                } else {
+                    T[index] = lambda_to_P[s_index]
                         .iter()
                         .fold(E::G1::zero().into(), |acc, P_index| {
                             acc.add(bases[*P_index]).into()
                         });
-                    if index != 0 {
-                        T[index] = T[index - 1];
-                    }
                 }
 
-                // Calculate the summation of T
-                let B_j = T
-                    .into_iter()
-                    .enumerate()
-                    .fold(E::G1::zero(), |acc, (lamada, acc_value)| {
-                        acc_value.mul(E::ScalarField::from(lamada as u128))
-                    });
+                if index != 0 {
+                    T[index] = T[index].add(T[index - 1].clone()).into();
+                }
+            }
 
-                let mut powers =  <<E as Pairing>::ScalarField as PrimeField>::BigInt::from(1_u64);
-                powers.muln(N_BIT as u32 * j as u32);
-                acc_B + B_j.mul(E::ScalarField::from_bigint(powers).unwrap())
-            });
+            // Calculate the summation of T
+            let B_j = T
+                .into_iter()
+                .enumerate()
+                .fold(E::G1::zero().into(), |acc, (lambda, acc_value)| {
+                    acc.add(acc_value).into()
+                });
 
-    Ok(result_g.into_affine())
+            let mut powers = <<E as Pairing>::ScalarField as PrimeField>::BigInt::from(1_u64);
+            powers.muln(N_BIT as u32 * j as u32);
+            (acc_B + B_j.mul(E::ScalarField::from_bigint(powers).unwrap())).into()
+        },
+    );
+
+    Ok(result_g)
 }
 
 trait FromBits {
     fn from_bits_le(value: &[bool]) -> Self;
 
-    fn to_bits_le(value: Self) ->  Vec<bool>;
+    fn to_bits_le(value: Self) -> Vec<bool>;
 }
 
 impl FromBits for u32 {
@@ -169,19 +183,19 @@ impl FromBits for u32 {
         })
     }
 
-    fn to_bits_le(value_in: u32) -> Vec<bool>{
+    fn to_bits_le(value_in: u32) -> Vec<bool> {
         let mut temp = Vec::new();
         let mut value = value_in.clone();
-        while  value !=0 {
+        while value != 0 {
             if value % 2 == 1 {
                 temp.push(true);
-            }else{
+            } else {
                 temp.push(false);
             }
-            value = value/2;
+            value = value / 2;
         }
 
-       temp
+        temp
     }
 }
 
@@ -199,7 +213,7 @@ pub fn split_to_nbit_uint_form<F: PrimeField>(value: F, nbit: usize) -> Vec<u32>
     // 2bit
     // split index 2 4 6
     let mut window_values: Vec<u32> = Vec::new();
-    println!("arr_len {}",arr.len());
+    println!("arr_len {}", arr.len());
     for index in 0..(arr.len() / nbit) {
         let mut t_arr = arr.clone();
         let window_bits: &[bool] = &t_arr.as_mut_slice()[index * nbit..(index + 1) * nbit];
@@ -244,11 +258,11 @@ pub fn split_to_nbit_form<F: PrimeField>(value: F, nbit: usize) -> Vec<BigInt<2>
 }
 
 pub fn Combine_nbit_form<F: PrimeField>(window_values: Vec<u32>, nbit: usize) -> F {
-    let mut res:Vec<BigInt<2>> = Vec::new();
-    window_values.into_iter().for_each(|v| {
-         res.push(BigInteger128::from(v))
-    });
-    combine_nbit_form(res,nbit)
+    let mut res: Vec<BigInt<2>> = Vec::new();
+    window_values
+        .into_iter()
+        .for_each(|v| res.push(BigInteger128::from(v)));
+    combine_nbit_form(res, nbit)
 }
 
 pub fn combine_nbit_form<F: PrimeField>(window_values: Vec<BigInt<2>>, nbit: usize) -> F {
@@ -265,42 +279,41 @@ pub fn combine_nbit_form<F: PrimeField>(window_values: Vec<BigInt<2>>, nbit: usi
     F::from_bigint(BigInteger::from_bits_le(&arr)).unwrap()
 }
 
-mod tests{
+mod tests {
     use super::*;
-    use basic::*;
     use ark_bls12_381::Bls12_381;
     use ark_bls12_381::Fr;
-    use rand::thread_rng;
-    use ark_ff::UniformRand;
-    use ark_ec::pairing::Pairing;
     use ark_ec::bls12::Bls12;
+    use ark_ec::pairing::Pairing;
+    use ark_ff::UniformRand;
+    use basic::*;
+    use rand::thread_rng;
 
     #[test]
-    fn test_split_and_combine_u32(){
+    fn test_split_and_combine_u32() {
         // binary representation
         // 1000
         // little
         // 0001
         let a: u32 = 8;
         let bits = u32::to_bits_le(a);
-        println!("{} {} {} {}",bits[0],bits[1],bits[2],bits[3]);
+        println!("{} {} {} {}", bits[0], bits[1], bits[2], bits[3]);
         // [0,0,0,1]
-        assert_eq!(bits[3],true);
+        assert_eq!(bits[3], true);
         let recover = u32::from_bits_le(bits.as_slice());
-        assert_eq!(recover,a);
+        assert_eq!(recover, a);
 
         let a: u32 = 11;
         let bits = u32::to_bits_le(a);
-        println!("{} {} {} {}",bits[0],bits[1],bits[2],bits[3]);
+        println!("{} {} {} {}", bits[0], bits[1], bits[2], bits[3]);
         // [0,0,0,1]
-        assert_eq!(bits[3],true);
+        assert_eq!(bits[3], true);
         let recover = u32::from_bits_le(bits.as_slice());
-        assert_eq!(recover,a);
+        assert_eq!(recover, a);
     }
 
     #[test]
     fn test_split_and_combine_PrimeField() {
-
         // let a = Fr::rand(&mut rng);
         let rng: &mut rand::rngs::ThreadRng = &mut thread_rng();
         let a = Fr::rand(rng);
@@ -310,21 +323,26 @@ mod tests{
         // assert_eq!(a.pow(&modulus), a); // the Euler-Fermat theorem tells us: a^{p-1} = 1 mod p
 
         let windows = split_to_nbit_uint_form(a, N_BIT);
-        println!("{} {} {} {} len:{}",windows[0],windows[1],windows[2],windows[3],windows.len());
+        println!(
+            "{} {} {} {} len:{}",
+            windows[0],
+            windows[1],
+            windows[2],
+            windows[3],
+            windows.len()
+        );
 
-        let res: Fr  = Combine_nbit_form(windows,N_BIT);
+        let res: Fr = Combine_nbit_form(windows, N_BIT);
 
         let e = res.eq(&a);
-        assert_eq!(e,true);
-
+        assert_eq!(e, true);
     }
 
     #[test]
-    fn test_pippenger(){
-
+    fn test_pippenger() {
         let rng: &mut rand::rngs::ThreadRng = &mut thread_rng();
-        let poly_degree = 32;
-        let params = BasicParameters::<Bls12_381>::new(poly_degree +2, rng).unwrap();
+        let poly_degree = 4;
+        let params = BasicParameters::<Bls12_381>::new(poly_degree + 1, rng).unwrap();
 
         // let poly = UniPolynomial::<Bls12_381>::new(vec![
         //     Fr::rand(rng),
@@ -340,18 +358,29 @@ mod tests{
 
         let poly = UniPolynomial::<Bls12_381>::new(vec![
             Fr::from(1),
+            Fr::from(20),
+            Fr::from(32),
             Fr::from(63),
-            Fr::from(4),
-            Fr::from(1),
+            // Fr::from(4),
+            // Fr::from(1),
         ]);
         let poly_vec1 = poly.clone().deref().to_vec();
         let poly_vec2 = poly.clone().deref().to_vec();
 
-        let v1= multiexp::<Bls12_381>(&params.clone().g1_vec(poly_degree + 2).unwrap(),poly_vec1).unwrap();
-        let v2= pippenger::<Bls12_381>(&params.g1_vec(poly_degree + 2).unwrap(),poly_vec2).unwrap();
-        assert_eq!(v1,v2);
-
-
+        let v1 = multiexp::<Bls12_381>(&params.clone().g1_vec(poly_degree + 1).unwrap(), poly_vec1)
+            .unwrap();
+        let v2 =
+            pippenger::<Bls12_381>(&params.g1_vec(poly_degree + 1).unwrap(), poly_vec2).unwrap();
+        assert_eq!(v1, v2);
     }
 
+    #[test]
+    fn test_from_bigInt() {}
+
+    #[test]
+    fn test_vec_map() {
+        let a = vec![1, 2, 3];
+        let b = a.clone().into_iter().map(|x| x + 3);
+        // println!("{}",b);
+    }
 }
